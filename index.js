52cc3eff7ba40513179a3391eb30f21b4b082c20
github username <github email address>
2021-09-27 14:40:47 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function findMinAndRemoveSorted(array) {
+    return array.shift()
+}
 
+function merge(firstArray, secondArray) {
+    let bigArray = []
+
+    while(firstArray.length > 0 && secondArray.length > 0) {
+        bigArray.push((firstArray[0] > secondArray[0]) ? findMinAndRemoveSorted(secondArray) : findMinAndRemoveSorted(firstArray))
+    }
+
+    return [...bigArray, ...firstArray, ...secondArray]
+}
+
+function mergeSort(array) {
+
+    let mid = array.length / 2
+    let firstHalf = array.slice(0, mid)
+    let secondHalf = array.slice(mid)
+
+    if(array.length < 2) {
+        return array
+    } else {
+        return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+}
+
+console.log(mergeSort([5, 2, 5, 2, 7]))
